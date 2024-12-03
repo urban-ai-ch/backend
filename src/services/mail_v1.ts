818faab0d1b5357a784e6f313f919517b4e153f9
Noah Gerber <noahgerber100@gmail.com>
2024-12-03 21:01:55 +0100

    add mail endpoints

--- src/services/mail_v1.ts
+++ src/services/mail_v1.ts
@@ -7,37 +7,49 @@ type MailPayload = {
 	message: string;
 };
 
-const recepients = ['noahgerber100@gmail.com', 'saimaneesh14@gmail.com'];
-
 const service: Service = {
 	path: '/mail/v1/',
 
 	fetch: async (request: Request, subPath: string, env: Env): Promise<Response | void> => {
-		switch (request.method + ' ' + subPath.split('/')[0]) {
-			case 'POST broadcast': {
-				const payload = await request.json<MailPayload>();
+		if (request.method !== 'POST') return;
 
-				const msg = createMimeMessage();
-				msg.setSender({ name: 'Backend', addr: 'backend@urban-ai.ch' });
-				msg.setRecipients(recepients);
-				msg.setSubject(payload.subject);
-				msg.addMessage({ contentType: 'text/plain', data: payload.message });
+		let recepients: string[] = [];
 
-				const mailPromises = recepients.map(async (recepient) => {
-					var message = new EmailMessage('backend@urban-ai.ch', `${recepient}`, msg.asRaw());
-
-					return env.EMAIL_BINDING.send(message).then(
-						() => `Mail to ${recepient} sent`,
-						(e) => `Mail to ${recepient} failed: ${e}`
-					);
-				});
-
-				return Promise.all(mailPromises).then(
-					(results) => new Response(JSON.stringify(results), { status: 201 }),
-					() => new Response('Internal server error', { status: 500 })
-				);
-			}
+		switch (subPath.split('/')[0]) {
+			case 'broadcast':
+				recepients = ['noahgerber100@gmail.com', 'saimaneesh14@gmail.com'];
+				break;
+			case 'noah':
+				recepients = ['noahgerber100@gmail.com'];
+				break;
+			case 'sai':
+				recepients = ['saimaneesh14@gmail.com'];
+				break;
+			default:
+				return;
 		}
+
+		const payload = await request.json<MailPayload>();
+
+		const msg = createMimeMessage();
+		msg.setSender({ name: 'Backend', addr: 'backend@urban-ai.ch' });
+		msg.setRecipients(recepients);
+		msg.setSubject(payload.subject);
+		msg.addMessage({ contentType: 'text/plain', data: payload.message });
+
+		const mailPromises = recepients.map(async (recepient) => {
+			var message = new EmailMessage('backend@urban-ai.ch', `${recepient}`, msg.asRaw());
+
+			return env.EMAIL_BINDING.send(message).then(
+				() => `Mail to ${recepient} sent`,
+				(e) => `Mail to ${recepient} failed: ${e}`
+			);
+		});
+
+		return Promise.all(mailPromises).then(
+			(results) => new Response(JSON.stringify(results), { status: 201 }),
+			() => new Response('Internal server error', { status: 500 })
+		);
 	},
 };
 

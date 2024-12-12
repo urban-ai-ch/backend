137697ee9696cb38679b29cbe555a41bd65085e1
Noah Gerber <noahgerber100@gmail.com>
2024-12-12 06:38:44 +0100

    remove redis/upstash

--- src/services/tokens_v1.ts
+++ src/services/tokens_v1.ts
@@ -1,6 +1,5 @@
 import { Service } from '..';
 import { authenticateToken } from './auth_v1';
-import { Redis } from '@upstash/redis';
 
 type OrderPayload = {
 	amount: number;
@@ -12,11 +11,6 @@ const service: Service = {
 	fetch: async (request: Request, subPath: string, env: Env): Promise<Response | void> => {
 		const authContext = await authenticateToken(request.headers, env);
 		if (authContext instanceof Response) return authContext;
-
-		const redis = new Redis({
-			url: 'https://aware-lionfish-49993.upstash.io',
-			token: env.UPSTASH_API_TOKEN,
-		});
 
 		switch (request.method + ' ' + subPath.split('/')[0]) {
 			case 'POST preview': {
